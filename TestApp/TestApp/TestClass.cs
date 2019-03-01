3b279f40d12ab4058d1328472f370e7cd0fc93c5
Paras <>
2019-03-01 13:04:06 +0530

    Added testClass

---
+++ TestApp/TestApp/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace TestApp
+{
+    public class TestClass
+    {
+        public TestClass()
+        {
+        }
+
+        public void testMethod()
+        {
+            Console.Write("Test");
+
+        }
+    }
+}
